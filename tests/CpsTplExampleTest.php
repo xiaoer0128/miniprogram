02b28734ca3fe6e9cd3a488d4894c20a156d6b7c
Timi <2811967514@qq.com>
2018-06-05 15:07:14 +0800

    first composer

---
+++ tests/CpsTplExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace Yin32167\CpsTpl\Tests;
+use PHPUnit\Framework\TestCase;
+final class CpsTplExampleTest extends TestCase 
+{
+    public function testCannotBeShow()
+    {
+        
+      echo 1;
+    }
+}
